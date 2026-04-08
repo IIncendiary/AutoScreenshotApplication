a0117b3c8592ebf1a154983f344f9047bec1833c
vmoskaltsev <vmoskaltsev.work@gmail.com>
2026-04-08 22:58:24 +0300

    Initial commit

---
+++ src/test/java/com/example/AutoScreenShotApplication/AutoScreenShotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AutoScreenShotApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoScreenShotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
